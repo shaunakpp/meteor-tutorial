99209c8375f582d21f02a9e383b5d3b9ff0c6fcb
Shaunak Pagnis <shaunak.pagnis@gmail.com>
2015-01-17 12:46:41 +0530

    add new tasks

--- meteor-tutorial.js
+++ meteor-tutorial.js
@@ -7,4 +7,23 @@ if (Meteor.isClient) {
       return Tasks.find({});
     }
   });
+
+  Template.body.events({
+    "submit .new-task": function (event) {
+      // This function is called when the new task form is submitted
+
+      var text = event.target.text.value;
+
+      Tasks.insert({
+        text: text,
+        createdAt: new Date() // current time
+      });
+
+      // Clear form
+      event.target.text.value = "";
+
+      // Prevent default form submit
+      return false;
+    }
+  });
 }
